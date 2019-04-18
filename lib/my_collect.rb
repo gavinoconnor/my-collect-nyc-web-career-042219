f2629ece822137366f0d3af00af3b0ec2af84952
gavinoconnor <gedmund@gmail.com>
2019-04-17 23:20:05 -0400

    Done

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  new_array = []
 
+  while i < array.length do
+    new_element = yield array[i]
+    new_array << new_element
+    i = i + 1
+  end
 
+  return new_array
+end
